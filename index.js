557d2d6914d21fc37205d2cf2c0efbb011d16689
SherryH <enjoy.wind7rain@gmail.com>
2017-01-23 15:01:29 -0800

    Served an index page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var path = require('path');
 
 //define an app
 var app = express();
@@ -10,12 +11,18 @@ var port = process.env.PORT || 3000;
 //parse all JSONs in requests
 app.use(bodyparser.json());
 
+//location to serve static files
+//place the default index.html file here too
+app.use(express.static(__dirname + '/public'));
+var publicPath = path.join(__dirname, '/public');
+
 //return Hello World
 app.get('/*', function(req, res) {
-  res.status(200).send('Hello Beautiful World');
+  res.status(200).sendFile(publicPath + '/index.html');
 });
 
 //start up server
 app.listen(port, function() {
   console.log('listening on port: ',port);
 });
+
