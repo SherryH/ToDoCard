fa8385c35078943c030a1444d5bfb938894fd792
SherryH <enjoy.wind7rain@gmail.com>
2017-01-23 12:25:22 -0800

    Simple server serving 'Hello World' completed

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+//define an app
+var app = express();
+
+//define port
+var port = process.env.PORT || 3000;
+
+//parse all JSONs in requests
+app.use(bodyparser.json());
+
+//return Hello World
+app.get('/', function(req, res) {
+  res.status(200).send('Hello World');
+});
+
+//start up server
+app.listen(port, function() {
+  console.log('listening on port: ',port);
+});
